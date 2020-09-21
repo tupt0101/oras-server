b05ba5b9b033254c037ccf22b7a75758caffb89b
leplep190599 <59325529+leplep190599@users.noreply.github.com>
2020-09-21 23:26:33 +0700

    Config DB

---
+++ src/test/java/capstone/oras/OrasApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.oras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
