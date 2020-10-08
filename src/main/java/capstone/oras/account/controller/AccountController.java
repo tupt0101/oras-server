3b699da63d632746402a15ccdaeef5d1d3288b2c
leplep190599 <59325529+leplep190599@users.noreply.github.com>
2020-10-08 18:43:14 +0700

    custom /oauth/token to /login (phucnlt)

--- src/main/java/capstone/oras/account/controller/AccountController.java
+++ src/main/java/capstone/oras/account/controller/AccountController.java
@@ -1,23 +1,20 @@
 package capstone.oras.account.controller;
 
-import capstone.oras.account.service.IAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
 
-    @Autowired
-    private IAccountService accountService;
+//    @Autowired
+//    private IAccountService accountService;
 
-    @RequestMapping(value = "/login")
-    @ResponseBody
-    String login() {
-        String token = accountService.login("admin@gmail.com","123456");
-        return token;
-    }
+//    @RequestMapping(value = "/login")
+//    @ResponseBody
+//    String login() {
+////        String token = accountService.login("admin@gmail.com","123456");
+////        return token;
+//
+//    }
 
 
 }
