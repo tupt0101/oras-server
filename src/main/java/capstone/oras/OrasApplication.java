ea22c709ab1d8caa8f7e072ba04167d70fba6853
leplep190599 <59325529+leplep190599@users.noreply.github.com>
2020-10-13 22:16:22 +0700

    fix EncodePassword

--- src/main/java/capstone/oras/OrasApplication.java
+++ src/main/java/capstone/oras/OrasApplication.java
@@ -2,6 +2,9 @@ package capstone.oras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,6 +14,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableResourceServer
 @EnableSwagger2
 public class OrasApplication {
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrasApplication.class, args);
