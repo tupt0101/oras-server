3f3f04f0eeda780b34ccf654ff5225b44f035719
AnhHY <yenanh2822@gmail.com>
2020-10-20 20:37:56 +0700

    fix config CORS

--- src/main/java/capstone/oras/oauth2/ResourceServerConfig.java
+++ src/main/java/capstone/oras/oauth2/ResourceServerConfig.java
@@ -25,7 +25,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                         ,"/swagger-ui**","/v2/api-docs", "/webjars/**"
                         ,"/swagger-resources/**","/configuration/**"
                         ,"/linkedin/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and().cors().and().csrf();
     }
 
 
