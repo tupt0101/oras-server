a246c950f2c20a74b2160d82a48248684aa28839
manbaxz123 <manvmse130081@fpt.edu.vn>
2020-11-04 15:21:54 +0700

    change  connection String

--- src/main/java/capstone/oras/oauth2/controller/LoginController.java
+++ src/main/java/capstone/oras/oauth2/controller/LoginController.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 @RestController
@@ -22,16 +22,16 @@ public class LoginController {
 //    @ResponseBody
 //    @CrossOrigin(origins = "http://localhost:8088")
     ResponseEntity<String>  login(@RequestParam("username") String email, @RequestParam("password") String password) throws Exception {
-//        String url = "http://localhost:8088/oauth/token";
-        String url = "https://oras-api.herokuapp.com/oauth/token";
+        String url = "http://localhost:8080/oauth/token";
+//        String url = "https://oras-api.herokuapp.com/oauth/token";
         URL obj = new URL(url);
-//        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-        HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+//        HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
         connection.setRequestMethod(RequestMethod.POST.name());
         connection.addRequestProperty("User-Agent",
                 "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-//        connection.setRequestProperty("Host", "localhost:8080");
-        connection.setRequestProperty("Host", "oras-api.herokuapp.com");
+        connection.setRequestProperty("Host", "localhost:8080");
+//        connection.setRequestProperty("Host", "oras-api.herokuapp.com");
         connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
         connection.setRequestProperty("Authorization", "Basic bXktdHJ1c3RlZC1jbGllbnQ6c2VjcmV0");
         String urlParameters = "grant_type=password&username="+ email + "&password=" + password;
