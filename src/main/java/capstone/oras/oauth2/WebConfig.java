9af111cc1f8fcc5bd313d2e89fb8a7c3cc1be5b1
manbaxz123 <manvmse130081@fpt.edu.vn>
2020-11-23 15:51:16 +0700

    cors

--- src/main/java/capstone/oras/oauth2/WebConfig.java
+++ src/main/java/capstone/oras/oauth2/WebConfig.java
@@ -1,14 +1,16 @@
 package capstone.oras.oauth2;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
+@Configuration
+@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedOrigins("*")
-                .allowedHeaders("Authorization", "Cache-Control", "Content-Type", "Accept", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
-                .exposedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin");
-    }
+    registry.addMapping("/**");
+ }
 }
