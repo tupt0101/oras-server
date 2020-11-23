cf5a548fcfbbd9259d1b45913eb9e7dadcdbfbaa
manbaxz123 <manvmse130081@fpt.edu.vn>
2020-11-23 14:33:54 +0700

    seperate api for tu instead of fixing the orignal

--- src/main/java/capstone/oras/oauth2/WebConfig.java
+++ src/main/java/capstone/oras/oauth2/WebConfig.java
@@ -1,12 +1,11 @@
 package capstone.oras.oauth2;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 public class WebConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+//    }
 }
