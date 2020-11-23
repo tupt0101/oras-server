658c39cb39393adba21f1c89c641c1efcb1de537
manbaxz123 <manvmse130081@fpt.edu.vn>
2020-11-23 15:55:48 +0700

    cors

--- src/main/java/capstone/oras/oauth2/WebConfig.java
+++ src/main/java/capstone/oras/oauth2/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    registry.addMapping("/**")
+    .allowedOrigins("https://oras-app.herokuapp.com");
  }
 }
