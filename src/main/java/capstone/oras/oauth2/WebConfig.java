675338579fbfa995169484a6a5abdcbdea75d2c7
manbaxz123 <manvmse130081@fpt.edu.vn>
2020-11-23 16:04:04 +0700

    cors

--- src/main/java/capstone/oras/oauth2/WebConfig.java
+++ src/main/java/capstone/oras/oauth2/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig  {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://oras-app.herokuapp.com")
                         .allowedMethods("GET", "POST", "OPTIONS", "PUT");
             }
         };
