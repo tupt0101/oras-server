21d47945e890b80106116ab0f4068794ddac2369
leplep190599 <59325529+leplep190599@users.noreply.github.com>
2020-11-28 21:23:47 +0700

    Update LoginController.java

--- src/main/java/capstone/oras/oauth2/controller/LoginController.java
+++ src/main/java/capstone/oras/oauth2/controller/LoginController.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 @RestController
