70a32917be6f31a2c7ee326043187de6b6a67d3c
leplep190599 <59325529+leplep190599@users.noreply.github.com>
2020-12-03 02:34:27 +0700

    login

--- src/main/java/capstone/oras/oauth2/controller/LoginController.java
+++ src/main/java/capstone/oras/oauth2/controller/LoginController.java
@@ -10,11 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 @RestController
@@ -38,11 +38,11 @@ public class LoginController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Account is not active");
         }
 
-        String url = "http://localhost:8080/oauth/token";
-//        String url = "https://oras-api.herokuapp.com/oauth/token";
+//        String url = "http://localhost:8080/oauth/token";
+        String url = "https://oras-api.herokuapp.com/oauth/token";
         URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-//        HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
+//        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+        HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
         connection.setRequestMethod(RequestMethod.POST.name());
         connection.addRequestProperty("User-Agent",
                 "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
