05bbce3cc7a62e8aee9558de3f428b2313f2a0c0
AnhHY <yenanh2822@gmail.com>
2020-12-21 03:17:35 +0700

    get notification for admin, read notification

--- src/main/java/capstone/oras/dao/INotificationRepository.java
+++ src/main/java/capstone/oras/dao/INotificationRepository.java
@@ -2,12 +2,19 @@ package capstone.oras.dao;
 
 import capstone.oras.entity.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public interface INotificationRepository extends JpaRepository<NotificationEntity, Integer> {
     Optional<List<NotificationEntity>> getNotificationEntitiesByNewTrueAndReceiverIdEquals(int receiverId);
     Optional<List<NotificationEntity>> getNotificationEntitiesByReceiverIdEquals(int receiverId);
+    @Query(value = "update notification set is_new = :isNew where id = :id", nativeQuery = true)
+    @Modifying
+    @Transactional
+    Integer updateIsNew(int id, boolean isNew);
 
 }
